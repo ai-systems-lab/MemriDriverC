3066aab38fd4021524fa3e6975b301d14eb8ee3a
rasberypi5 <rasberypi5@email.ru>
2025-05-10 12:50:50 +0300

    add gitignore and error

--- kurs_lib/setup.py
+++ kurs_lib/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, Extension
 
 module = Extension(
-    'gpio_с', # отвечает за то что пишется в import
+    'gpio', # отвечает за то что пишется в import
     sources=['gpio_module.c'],
     libraries=['gpiod'],  # Указываем линковку с libgpiod
 )
 
 setup(
     name='gpio_с', # имя пакета
-    version='0.1.0', # версия
+    version='1.0', # версия
     description='GPIO модуль для Raspberry Pi на С', # описание
     ext_modules=[module],
 )
