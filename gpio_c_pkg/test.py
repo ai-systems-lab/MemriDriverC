047d41ceae11d8e42fe79826671f837799c1227e
rasberypi5 <rasberypi5@email.ru>
2025-05-10 15:12:00 +0300

    fix test

--- gpio_c_pkg/test.py
+++ gpio_c_pkg/test.py
@@ -5,7 +5,10 @@ gpio.init()
 
 line = gpio.setup(17)
 print(f"GPIO 17 line: {line}")
-while True:        
-    gpio.set(line, 1)
-    time.sleep(2)
-    gpio.set(line, 0)
+    
+    
+gpio.set(line, 1)
+time.sleep(2)
+gpio.set(line, 0)
+
+gpio.deinit()
