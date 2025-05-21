ac017ddf02c21a452207c585f1f23e89d8bb86b4
elyso <fantom_work@bk.ru>
2025-05-21 20:21:03 +0300

    maybe full rewrite

--- lib_c/rewrited/mvmdriver_wrapper.py
+++ lib_c/rewrited/mvmdriver_wrapper.py
@@ -5,7 +5,7 @@ import os
 lib_path = os.path.join(os.path.dirname(__file__), 'libmvmdriver.so')
 mvmdriver = CDLL(lib_path)
 
-# Определяем структуры для соответствия C-структурам
+# Определяем структуры
 class RegControl595(Structure):
     _fields_ = [("main_bytes", c_ubyte * 2)]
 
@@ -17,7 +17,7 @@ class RPI_modes(Structure):
     _fields_ = [("mvm_spi", SPI_send),
                 ("reg", RegControl595)]
 
-# Инициализация функций
+# Привязки функций
 mvmdriver.RPI_modes_init.argtypes = [POINTER(RPI_modes)]
 mvmdriver.RPI_modes_init.restype = None
 
@@ -25,7 +25,25 @@ mvmdriver.mode_7.argtypes = [POINTER(RPI_modes), c_uint16, c_uint16, c_uint16,
                             c_uint8, c_uint16, c_uint8, c_uint8, POINTER(c_uint16)]
 mvmdriver.mode_7.restype = None
 
-# Класс-обертка для удобной работы из Python
+mvmdriver.mode_9.argtypes = [POINTER(RPI_modes), c_uint16, c_uint16, c_uint8, c_uint8, 
+                            POINTER(c_uint16)]
+mvmdriver.mode_9.restype = None
+
+mvmdriver.mode_mvm.argtypes = [POINTER(RPI_modes), POINTER(c_uint16), c_uint16, c_uint16, 
+                              c_uint16, c_uint16, c_uint8, c_uint16, POINTER(c_uint16)]
+mvmdriver.mode_mvm.restype = None
+
+mvmdriver.fast_mvm_ON.argtypes = [POINTER(RPI_modes)]
+mvmdriver.fast_mvm_ON.restype = None
+
+mvmdriver.fast_mvm_OFF.argtypes = [POINTER(RPI_modes)]
+mvmdriver.fast_mvm_OFF.restype = None
+
+mvmdriver.fast_mvm.argtypes = [POINTER(RPI_modes), POINTER(c_uint16), c_uint16, c_uint16, 
+                              c_uint16, c_uint16, c_uint8, c_uint16, POINTER(c_uint16)]
+mvmdriver.fast_mvm.restype = None
+
+# Класс-обёртка
 class MVMDriver:
     def __init__(self):
         self.rpi = RPI_modes()
@@ -35,3 +53,44 @@ class MVMDriver:
         result = c_uint16(0)
         mvmdriver.mode_7(byref(self.rpi), vDAC, tms, tus, rev, id, wl, bl, byref(result))
         return result.value, id
+    
+    def mode_9(self, vDAC, id, wl, bl):
+        result = c_uint16(0)
+        mvmdriver.mode_9(byref(self.rpi), vDAC, id, wl, bl, byref(result))
+        return result.value, id
+    
+    def mode_mvm(self, vDAC_mas, tms, tus, rtms, rums, wl, id):
+        # Преобразуем Python-список в C-массив
+        if len(vDAC_mas) != 32:
+            raise ValueError("vDAC_mas должен содержать ровно 32 элемента")
+        vDAC_mas_array = (c_uint16 * 32)(*vDAC_mas)
+        result = c_uint16(0)
+        mvmdriver.mode_mvm(byref(self.rpi), vDAC_mas_array, tms, tus, rtms, rums, 
+                          wl, id, byref(result))
+        return result.value, id
+    
+    def fast_mvm_ON(self):
+        mvmdriver.fast_mvm_ON(byref(self.rpi))
+    
+    def fast_mvm_OFF(self):
+        mvmdriver.fast_mvm_OFF(byref(self.rpi))
+    
+    def fast_mvm(self, vDAC_mas, tms, tus, rtms, rums, wl, id):
+        # Преобразуем Python-список в C-массив
+        if len(vDAC_mas) != 32:
+            raise ValueError("vDAC_mas должен содержать ровно 32 элемента")
+        vDAC_mas_array = (c_uint16 * 32)(*vDAC_mas)
+        result = c_uint16(0)
+        mvmdriver.fast_mvm(byref(self.rpi), vDAC_mas_array, tms, tus, rtms, rums, 
+                          wl, id, byref(result))
+        return result.value, id
+
+if __name__ == "__main__":
+    driver = MVMDriver()
+    # Тест mode_7
+    result, id = driver.mode_7(245, 1, 0, 0, 123, 0, 0)
+    print(f"mode_7 result: {result}, id: {id}")
+    # Тест mode_mvm
+    vDAC_mas = [100] * 32  # Пример массива
+    result, id = driver.mode_mvm(vDAC_mas, 1, 0, 0, 0, 0, 123)
+    print(f"mode_mvm result: {result}, id: {id}")
--- lib_c/rewrited/rpi_models.c
+++ lib_c/rewrited/rpi_models.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
-#include <math.h>
 #include "r595hc.c"
 #include "MVM_SPI.c"
 
@@ -25,7 +24,7 @@ void RPI_modes_init(RPI_modes *rpi) {
 }
 
 void mode_7(RPI_modes *rpi, uint16_t vDAC, uint16_t tms, uint16_t tus, uint8_t rev, 
-            uint16_t id, uint8_t wl, uint8_t bl, uint16_t *result) {
+            uint16_t id, uint8_t wl, uint8_t bl, uint16_t *result, uint16_t *ret_id) {
     // Off MVM DACs
     for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
     mwm_dac_pd_on(&rpi->mvm_spi);
@@ -46,11 +45,11 @@ void mode_7(RPI_modes *rpi, uint16_t vDAC, uint16_t tms, uint16_t tus, uint8_t r
     }
     
     // Set commutation WR for bl
-    if (bl <= 15) {
+    if (bl >= 0 && bl <= 15) {
         wr_spdt_comm_L(&rpi->reg);
         in_mux_EN_H(&rpi->reg, 0);
         in_mux_EN_L(&rpi->reg, 1);
-    } else if (bl <= 31) {
+    } else if (bl >= 16 && bl <= 31) {
         wr_spdt_comm_H(&rpi->reg);
         in_mux_EN_L(&rpi->reg, 0);
         in_mux_EN_H(&rpi->reg, 1);
@@ -63,7 +62,7 @@ void mode_7(RPI_modes *rpi, uint16_t vDAC, uint16_t tms, uint16_t tus, uint8_t r
     reg_update(&rpi->reg);
     
     // Set commutation wl
-    if (wl <= 7) {
+    if (wl >= 0 && wl <= 7) {
         digitalWrite(17, (wl & 0b1) ? HIGH : LOW);
         digitalWrite(27, (wl >> 1 & 0b1) ? HIGH : LOW);
         digitalWrite(22, (wl >> 2 & 0b1) ? HIGH : LOW);
@@ -133,7 +132,7 @@ void mode_7(RPI_modes *rpi, uint16_t vDAC, uint16_t tms, uint16_t tus, uint8_t r
     
     // Read from ADC
     *result = 0;
-    spdt_select_mode_for_ADC_wr();
+    spdt_select_mode_for_ADC_wr(&rpi->reg);
     set_mode_1(&rpi->mvm_spi);
     digitalWrite(25, LOW);
     adc_read(&rpi->mvm_spi);
@@ -194,6 +193,368 @@ void mode_7(RPI_modes *rpi, uint16_t vDAC, uint16_t tms, uint16_t tus, uint8_t r
     // LDAC
     digitalWrite(4, LOW);
     digitalWrite(4, HIGH);
+    
+    *ret_id = id;
+}
+
+void mode_9(RPI_modes *rpi, uint16_t vDAC, uint16_t id, uint8_t wl, uint8_t bl, 
+            uint16_t *result, uint16_t *ret_id) {
+    // Off MVM DACs
+    for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
+    mwm_dac_pd_on(&rpi->mvm_spi);
+    for (int i = 5; i <= 8; i++) digitalWrite(i, HIGH);
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Open all keys 714
+    for (int i = 0; i < 4; i++) {
+        bl_key_cs_L(&rpi->reg, i);
+        reg_update(&rpi->reg);
+        set_mode_1(&rpi->mvm_spi);
+        key_set_MVM_off(&rpi->mvm_spi);
+        bl_key_cs_H(&rpi->reg, i);
+        reg_update(&rpi->reg);
+    }
+    
+    // Set commutation WR for bl
+    if (bl >= 0 && bl <= 15) {
+        wr_spdt_comm_L(&rpi->reg);
+        in_mux_EN_H(&rpi->reg, 0);
+        in_mux_EN_L(&rpi->reg, 1);
+    } else if (bl >= 16 && bl <= 31) {
+        wr_spdt_comm_H(&rpi->reg);
+        in_mux_EN_L(&rpi->reg, 0);
+        in_mux_EN_H(&rpi->reg, 1);
+        bl -= 16;
+    }
+    in_mux_set(&rpi->reg, bl);
+    
+    // Set mode to WR on wl mux spdt
+    spdt_select_mode_L(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    // Set commutation wl
+    if (wl >= 0 && wl <= 7) {
+        digitalWrite(17, (wl & 0b1) ? HIGH : LOW);
+        digitalWrite(27, (wl >> 1 & 0b1) ? HIGH : LOW);
+        digitalWrite(22, (wl >> 2 & 0b1) ? HIGH : LOW);
+        digitalWrite(13, HIGH);
+    } else {
+        printf("Wrong WL!\n");
+    }
+    
+    // Set reading direction
+    wr_spdt_L(&rpi->reg, 0);
+    wr_spdt_L(&rpi->reg, 1);
+    wr_spdt_H(&rpi->reg, 2);
+    reg_update(&rpi->reg);
+    
+    // Set ZERO on WR DAC
+    if (vDAC > 246) vDAC = 246;
+    wr_dac_cs_L(&rpi->reg);
+    reg_update(&rpi->reg);
+    set_mode_0(&rpi->mvm_spi);
+    wr_dac(&rpi->mvm_spi, vDAC);
+    wr_dac_cs_H(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    // Update WR DAC value with LDAC WR
+    digitalWrite(12, LOW);
+    digitalWrite(12, HIGH);
+    
+    // Read from ADC
+    spdt_select_mode_for_ADC_wr(&rpi->reg);
+    set_mode_1(&rpi->mvm_spi);
+    digitalWrite(25, LOW);
+    adc_read(&rpi->mvm_spi);
+    digitalWrite(25, HIGH);
+    
+    digitalWrite(25, LOW);
+    *result = adc_read(&rpi->mvm_spi);
+    digitalWrite(25, HIGH);
+    
+    // Set ZERO on WR DAC
+    wr_dac_cs_L(&rpi->reg);
+    reg_update(&rpi->reg);
+    set_mode_0(&rpi->mvm_spi);
+    wr_dac(&rpi->mvm_spi, 0);
+    wr_dac_cs_H(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    // Update WR DAC value with LDAC WR
+    digitalWrite(12, LOW);
+    digitalWrite(12, HIGH);
+    
+    // Set dir direction
+    wr_spdt_L(&rpi->reg, 0);
+    wr_spdt_L(&rpi->reg, 1);
+    wr_spdt_L(&rpi->reg, 2);
+    reg_update(&rpi->reg);
+    
+    // Disable commutation
+    digitalWrite(13, LOW);
+    digitalWrite(17, LOW);
+    digitalWrite(27, LOW);
+    digitalWrite(22, LOW);
+    wr_spdt_comm_L(&rpi->reg);
+    in_mux_EN_L(&rpi->reg, 0);
+    in_mux_EN_L(&rpi->reg, 1);
+    in_mux_set(&rpi->reg, 0);
+    spdt_select_mode_L(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    // Open all keys 714
+    for (int i = 0; i < 4; i++) {
+        bl_key_cs_L(&rpi->reg, i);
+        reg_update(&rpi->reg);
+        set_mode_1(&rpi->mvm_spi);
+        key_set_MVM_off(&rpi->mvm_spi);
+        bl_key_cs_H(&rpi->reg, i);
+        reg_update(&rpi->reg);
+    }
+    
+    // On MVM DACs
+    for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
+    mwm_dac_pd_off(&rpi->mvm_spi);
+    for (int i = 5; i <= 8; i++) digitalWrite(i, HIGH);
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    *ret_id = id;
+}
+
+void mode_mvm(RPI_modes *rpi, uint16_t *vDAC_mas, uint16_t tms, uint16_t tus, 
+              uint16_t rtms, uint16_t rums, uint8_t wl, uint16_t id, 
+              uint16_t *result, uint16_t *ret_id) {
+    // On MVM DACs
+    for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
+    set_mode_1(&rpi->mvm_spi);
+    mwm_dac_pd_off(&rpi->mvm_spi);
+    for (int i = 5; i <= 8; i++) digitalWrite(i, HIGH);
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Close all keys 714 based on vDAC_mas mask
+    uint8_t mask[32];
+    for (int i = 0; i < 32; i++) {
+        mask[i] = vDAC_mas[i] > 0 ? 1 : 0;
+    }
+    uint8_t mask_bytes[4] = {0};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 8; j++) {
+            mask_bytes[i] |= (mask[i * 8 + j] << j);
+        }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        bl_key_cs_L(&rpi->reg, i);
+        reg_update(&rpi->reg);
+        set_mode_1(&rpi->mvm_spi);
+        key_set_MVM_on_mask(&rpi->mvm_spi, mask_bytes[i]);
+        bl_key_cs_H(&rpi->reg, i);
+        reg_update(&rpi->reg);
+    }
+    
+    // Disable commutation
+    digitalWrite(13, LOW);
+    digitalWrite(17, LOW);
+    digitalWrite(27, LOW);
+    digitalWrite(22, LOW);
+    wr_spdt_comm_L(&rpi->reg);
+    in_mux_EN_L(&rpi->reg, 0);
+    in_mux_EN_L(&rpi->reg, 1);
+    in_mux_set(&rpi->reg, 0);
+    spdt_select_mode_H(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    // Set commutation wl
+    if (wl >= 0 && wl <= 7) {
+        digitalWrite(17, (wl & 0b1) ? HIGH : LOW);
+        digitalWrite(27, (wl >> 1 & 0b1) ? HIGH : LOW);
+        digitalWrite(22, (wl >> 2 & 0b1) ? HIGH : LOW);
+        digitalWrite(13, HIGH);
+    } else {
+        printf("Wrong WL!\n");
+    }
+    
+    // Set values to MVM DACs
+    for (int i = 0; i < 4; i++) {
+        set_mode_1(&rpi->mvm_spi);
+        for (int j = 0; j < 8; j++) {
+            digitalWrite(i + 5, LOW);
+            mvm_dac(&rpi->mvm_spi, vDAC_mas[j + (8 * i)], j);
+            digitalWrite(i + 5, HIGH);
+        }
+    }
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Read from ADC
+    spdt_select_mode_for_ADC_mvm(&rpi->reg);
+    set_mode_1(&rpi->mvm_spi);
+    digitalWrite(25, LOW);
+    adc_read(&rpi->mvm_spi);
+    digitalWrite(25, HIGH);
+    
+    digitalWrite(25, LOW);
+    *result = adc_read(&rpi->mvm_spi);
+    digitalWrite(25, HIGH);
+    
+    spdt_select_mode_for_ADC_wr(&rpi->reg);
+    
+    // Set ZERO to MVM DACs
+    set_mode_1(&rpi->mvm_spi);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 8; j++) {
+            digitalWrite(i + 5, LOW);
+            mvm_dac(&rpi->mvm_spi, 0, j);
+            digitalWrite(i + 5, HIGH);
+        }
+    }
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Off MVM DACs
+    for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
+    set_mode_1(&rpi->mvm_spi);
+    mwm_dac_pd_on(&rpi->mvm_spi);
+    for (int i = 5; i <= 8; i++) digitalWrite(i, HIGH);
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Disable commutation
+    digitalWrite(13, LOW);
+    digitalWrite(17, LOW);
+    digitalWrite(27, LOW);
+    digitalWrite(22, LOW);
+    spdt_select_mode_H(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    *ret_id = id;
+}
+
+void fast_mvm_ON(RPI_modes *rpi) {
+    // On MVM DACs
+    for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
+    set_mode_1(&rpi->mvm_spi);
+    mwm_dac_pd_off(&rpi->mvm_spi);
+    for (int i = 5; i <= 8; i++) digitalWrite(i, HIGH);
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Close all keys 714
+    for (int i = 0; i < 4; i++) {
+        bl_key_cs_L(&rpi->reg, i);
+        reg_update(&rpi->reg);
+        set_mode_1(&rpi->mvm_spi);
+        key_set_MVM_on(&rpi->mvm_spi);
+        bl_key_cs_H(&rpi->reg, i);
+        reg_update(&rpi->reg);
+    }
+    
+    // Disable commutation
+    digitalWrite(13, LOW);
+    digitalWrite(17, LOW);
+    digitalWrite(27, LOW);
+    digitalWrite(22, LOW);
+    wr_spdt_comm_L(&rpi->reg);
+    in_mux_EN_L(&rpi->reg, 0);
+    in_mux_EN_L(&rpi->reg, 1);
+    in_mux_set(&rpi->reg, 0);
+    spdt_select_mode_H(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    spdt_select_mode_for_ADC_mvm(&rpi->reg);
+}
+
+void fast_mvm_OFF(RPI_modes *rpi) {
+    // Off MVM DACs
+    for (int i = 5; i <= 8; i++) digitalWrite(i, LOW);
+    set_mode_1(&rpi->mvm_spi);
+    mwm_dac_pd_on(&rpi->mvm_spi);
+    for (int i = 5; i <= 8; i++) digitalWrite(i, HIGH);
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Disable commutation
+    digitalWrite(13, LOW);
+    digitalWrite(17, LOW);
+    digitalWrite(27, LOW);
+    digitalWrite(22, LOW);
+    spdt_select_mode_H(&rpi->reg);
+    reg_update(&rpi->reg);
+    
+    spdt_select_mode_for_ADC_wr(&rpi->reg);
+}
+
+void fast_mvm(RPI_modes *rpi, uint16_t *vDAC_mas, uint16_t tms, uint16_t tus, 
+              uint16_t rtms, uint16_t rums, uint8_t wl, uint16_t id, 
+              uint16_t *result, uint16_t *ret_id) {
+    // Set commutation wl
+    if (wl >= 0 && wl <= 7) {
+        digitalWrite(17, (wl & 0b1) ? HIGH : LOW);
+        digitalWrite(27, (wl >> 1 & 0b1) ? HIGH : LOW);
+        digitalWrite(22, (wl >> 2 & 0b1) ? HIGH : LOW);
+        digitalWrite(13, HIGH);
+    } else {
+        printf("Wrong WL!\n");
+    }
+    
+    // Set values to MVM DACs
+    for (int i = 0; i < 4; i++) {
+        set_mode_1(&rpi->mvm_spi);
+        for (int j = 0; j < 8; j++) {
+            digitalWrite(i + 5, LOW);
+            mvm_dac(&rpi->mvm_spi, vDAC_mas[j + (8 * i)], j);
+            digitalWrite(i + 5, HIGH);
+        }
+    }
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    // Read from ADC
+    set_mode_1(&rpi->mvm_spi);
+    digitalWrite(25, LOW);
+    adc_read(&rpi->mvm_spi);
+    digitalWrite(25, HIGH);
+    
+    digitalWrite(25, LOW);
+    *result = adc_read(&rpi->mvm_spi);
+    digitalWrite(25, HIGH);
+    
+    // Set ZERO to MVM DACs
+    set_mode_1(&rpi->mvm_spi);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 8; j++) {
+            digitalWrite(i + 5, LOW);
+            mvm_dac(&rpi->mvm_spi, 0, j);
+            digitalWrite(i + 5, HIGH);
+        }
+    }
+    
+    // LDAC
+    digitalWrite(4, LOW);
+    digitalWrite(4, HIGH);
+    
+    *ret_id = id;
 }
 
 int main() {
@@ -202,9 +563,10 @@ int main() {
     RPI_modes rpi;
     RPI_modes_init(&rpi);
     
-    uint16_t result;
-    mode_7(&rpi, 245, 1, 0, 0, 123, 0, 0, &result);
-    printf("Result: %d\n", result);
+    uint16_t result, ret_id;
+    mode_7(&rpi, 245, 1, 0, 0, 123, 0, 0, &result, &ret_id);
+    printf("Result: %d, ID: %d\n", result, ret_id);
     
     return 0;
 }
+//gcc -shared -o libmvmdriver.so -fPIC rpi_modes.c MVM_SPI.c r595hc.c -lwiringPi
